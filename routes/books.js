9266233bf4368e530cba9396a1ebc5c5716d1b36
jeong922 <hwajeong922@naver.com>
2024-04-11 15:42:50 +0900

    routes/books.js 유효성 검사 구현

--- routes/books.js
+++ routes/books.js
@@ -1,14 +1,29 @@
 import express from 'express';
 import * as bookController from '../controller/books.js';
+import { param, query } from 'express-validator';
+import { validate } from '../middleware/validator.js';
 
 const router = express.Router();
+
+const validateQuery = [
+  query('categoryId').isInt().withMessage('categoryId를 숫자로 입력해 주세요.'),
+  query('newBook')
+    .isBoolean()
+    .withMessage('true 또는 false로만 입력해 주새요.'),
+  validate,
+];
+
+const validateId = [
+  param('id').notEmpty().isInt().withMessage('id를 숫자로 입력해주세요.'),
+  validate,
+];
 
 // FIXME: 페이징 구현 필요
 
 // 전체 조회 -> req.query가 있는 경우 카테고리별 조회
-router.get('/', bookController.allBooks);
+router.get('/', validateQuery, bookController.allBooks);
 
 // 개별 조회
-router.get('/:id', bookController.bookDetail);
+router.get('/:id', validateId, bookController.bookDetail);
 
 export default router;
