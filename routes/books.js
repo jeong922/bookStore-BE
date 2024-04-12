b37d5b7b224824e1ba2851ac31bb2deffbaf9fdf
jeong922 <hwajeong922@naver.com>
2024-04-12 14:50:52 +0900

    도서 API : 유효성 검사 수정

--- routes/books.js
+++ routes/books.js
@@ -14,8 +14,11 @@ const validateQuery = [
     .isBoolean()
     .optional()
     .withMessage('true 또는 false로만 입력해 주새요.'),
-  query('maxResults').isInt().withMessage('maxResults를 숫자로 입력해 주세요.'),
-  query('page').isInt().withMessage('page를 숫자로 입력해 주세요.'),
+  query('maxResults')
+    .notEmpty()
+    .isInt()
+    .withMessage('maxResults를 숫자로 입력해 주세요.'),
+  query('page').notEmpty().isInt().withMessage('page를 숫자로 입력해 주세요.'),
   validate,
 ];
 
