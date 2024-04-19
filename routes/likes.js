3a96d138c624bbdaa996e1b912d97d1aec01d351
jeong922 <hwajeong922@naver.com>
2024-04-19 19:08:02 +0900

    좋아요 API : path 수정 및 userId req.body로 받아오게 수정

--- routes/likes.js
+++ routes/likes.js
@@ -4,9 +4,9 @@ import * as likeController from '../controller/likes.js';
 const router = express.Router();
 
 // 좋아요 추가
-router.post('/:userId/:bookId', likeController.likeBook);
+router.post('/:bookId', likeController.likeBook);
 
 // 좋아요 삭제
-router.delete('/:userId/:bookId', likeController.dislikeBook);
+router.delete('/:bookId', likeController.dislikeBook);
 
 export default router;
