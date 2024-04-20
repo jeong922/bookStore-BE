c1d5b4c85d0eb80f274b1a69a4fa74c2aeba90ea
jeong922 <hwajeong922@naver.com>
2024-04-20 15:37:53 +0900

    검색 API : 유효성 검사 추가

--- routes/search.js
+++ routes/search.js
@@ -1,9 +1,31 @@
 import express from 'express';
 import * as searchController from '../controller/search.js';
+import { query } from 'express-validator';
+import { validate } from '../middleware/validator.js';
 
 const router = express.Router();
 
+const validateKeyword = [
+  query('keyword')
+    .notEmpty()
+    .withMessage('검색어를 입력해주세요.')
+    .trim()
+    .isLength({ min: 1 })
+    .withMessage('최소 한 글자 이상 입력해주세요.'),
+  query('maxResults')
+    .notEmpty()
+    .withMessage('숫자로 입력해 주세요.')
+    .isNumeric()
+    .withMessage('숫자로 입력해 주세요.'),
+  query('page')
+    .notEmpty()
+    .withMessage('숫자로 입력해 주세요.')
+    .isNumeric()
+    .withMessage('숫자로 입력해 주세요.'),
+  validate,
+];
+
 // 키워드 검색
-router.get('/', searchController.searchKeyword);
+router.get('/', validateKeyword, searchController.searchKeyword);
 
 export default router;
