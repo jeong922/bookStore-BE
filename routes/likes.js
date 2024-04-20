ad7a664690dd607fc4ccd751b6744484bbce55bf
jeong922 <hwajeong922@naver.com>
2024-04-20 15:45:58 +0900

    좋아요 API : 유효성 검사 추가

--- routes/likes.js
+++ routes/likes.js
@@ -1,12 +1,19 @@
 import express from 'express';
 import * as likeController from '../controller/likes.js';
+import { param } from 'express-validator';
+import { validate } from '../middleware/validator.js';
 
 const router = express.Router();
 
+const validateLike = [
+  param('bookId').notEmpty().isInt().withMessage('숫자로 입력해 주세요.'),
+  validate,
+];
+
 // 좋아요 추가
-router.post('/:bookId', likeController.likeBook);
+router.post('/:bookId', validateLike, likeController.likeBook);
 
 // 좋아요 삭제
-router.delete('/:bookId', likeController.dislikeBook);
+router.delete('/:bookId', validateLike, likeController.dislikeBook);
 
 export default router;
