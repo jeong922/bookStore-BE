47fa21cc17cb967fcc0a540772891e108e78d26c
jeong922 <hwajeong922@naver.com>
2024-04-20 16:23:48 +0900

    리뷰 API :  유효성 검사 추가

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,21 +1,50 @@
 import express from 'express';
 import * as reviewController from '../controller/reviews.js';
+import { body, param } from 'express-validator';
+import { validate } from '../middleware/validator.js';
 
 const router = express.Router();
 
+const validateBookId = [
+  param('bookId').notEmpty().isInt().withMessage('숫자로 입력해 주세요.'),
+  validate,
+];
+
+const validateReview = [
+  body('userId').notEmpty().isInt().withMessage('숫자로 입력해 주세요.'),
+  body('bookId').notEmpty().isInt().withMessage('숫자로 입력해 주세요.'),
+  body('text')
+    .notEmpty()
+    .trim()
+    .isLength({ min: 1 })
+    .withMessage('최소 한 글자 이상 입력해 주세요.'),
+  validate,
+];
+
+const validateRemoveReview = [
+  body('userId').notEmpty().isInt().withMessage('숫자로 입력해 주세요.'),
+  body('reviewId').notEmpty().isInt().withMessage('숫자로 입력해 주세요.'),
+  validate,
+];
+
+const validateUserId = [
+  body('userId').notEmpty().isInt().withMessage('숫자로 입력해 주세요.'),
+  validate,
+];
+
 // 도서별 리뷰 조회
-router.get('/:bookId', reviewController.getReviews);
+router.get('/:bookId', validateBookId, reviewController.getReviews);
 
 // 리뷰 작성
-router.post('/', reviewController.addReview);
+router.post('/', validateReview, reviewController.addReview);
 
 // 리뷰 수정
-router.put('/', reviewController.updateReview);
+router.put('/', validateReview, reviewController.updateReview);
 
 // 리뷰 삭제
-router.delete('/', reviewController.removeReview);
+router.delete('/', validateRemoveReview, reviewController.removeReview);
 
 // 리뷰 회원별 조회
-router.get('/', reviewController.getUserReviews);
+router.get('/', validateUserId, reviewController.getUserReviews);
 
 export default router;
