8288f6ad4ef879a92f2db4bb01fb8f11b94bdb54
jeong922 <hwajeong922@naver.com>
2024-04-23 15:56:20 +0900

    인증 미들웨어 optionalEnsureAuth 수정

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -48,19 +48,37 @@ export async function optionalEnsureAuth(
   next: NextFunction
 ) {
   const authHeader = req.get('Authorization');
-  if (!(authHeader && authHeader.startsWith('Bearer '))) {
-    return res.status(StatusCodes.UNAUTHORIZED).json({ message: '인증 오류' });
-  }
-  const token = authHeader.split(' ')[1];
-
   try {
-    const decodedJwt = jwt.verify(token, config.jwt.secretKey) as JwtPayload;
+    if (!authHeader || !(authHeader && authHeader.startsWith('Bearer '))) {
+      // throw new ReferenceError('jwt must be provided');
+      next();
+      return;
+    }
 
-    req.userId = decodedJwt.id;
+    if (authHeader && authHeader.startsWith('Bearer ')) {
+      const token = authHeader.split(' ')[1];
+      const decodedJwt = jwt.verify(token, config.jwt.secretKey) as JwtPayload;
 
-    return next();
+      const user = await getByUserId(decodedJwt.id);
+
+      if (!user) {
+        return res
+          .status(StatusCodes.UNAUTHORIZED)
+          .json({ message: '인증 오류' });
+      }
+
+      req.userId = decodedJwt.id;
+      next();
+    }
   } catch (err) {
-    // 오류 무시
+    if (err instanceof jwt.TokenExpiredError) {
+      return res
+        .status(StatusCodes.UNAUTHORIZED)
+        .json({ message: '로그인 세션이 만료되었습니다.' });
+    }
+
+    return res
+      .status(StatusCodes.UNAUTHORIZED)
+      .json({ message: '유효하지 않은 토큰입니다.' });
   }
-  next();
 }
